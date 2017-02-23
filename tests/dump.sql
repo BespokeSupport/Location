409747dc79c5c5d81fcd3d6be216422fa23b7f2f
Richard Seymour <web@bespoke.support>
2017-02-23 09:16:43 +0000

    typo in sql

--- tests/dump.sql
+++ tests/dump.sql
@@ -4,4 +4,4 @@ SET foreign_key_checks = 0;
 USE `test`;
 
 INSERT INTO `postcode_outwards` (`postcode_outward`, `postcode_area`, `outward_part`, `latitude`, `longitude`) VALUES
-('AB10',	'AB',	'10'	57.1351,	-2.1173);
+('AB10',	'AB',	'10',	57.1351,	-2.1173);
